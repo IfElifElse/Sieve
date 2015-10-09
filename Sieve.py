3928458f833681b74c449d15c4eb666a7d44486b
IfElifElse <carlthechimchar@gmail.com>
2015-10-08 20:03:45 -0400

    Sieve 1.1.0
    Fixed the broken interpreter. Works like a charm now.

--- Sieve.py
+++ Sieve.py
@@ -5,68 +5,90 @@ script, file = sys.argv
 
 loops, tape, tapeslct, cmdslct = [], [0], 0, 0
 code = open(file)
-program = filter(lambda x: x in ['.', ':', ',', ';', '[', ']', '<', '>', '+', '-', '|', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'], code.read())
-program = list(program)
-condparse = 0
-condition = []
-loop = 0
+program = code.read()
+stdin = list(input("Input: "))
 
-debug = 0
+debug = 1
+
+if debug == 1: print(program)
 
 def execute(cmd):
     global tape
     global tapeslct
     global cmdslct
-    global condparse
-    global condition
-    global debug
-    global loop
-    if condparse == 1:
-        if cmd != "|":
-            condition.append(cmd)
-            if debug == 1:
-                print condition
-        else:
-            condparse = 0
-            cond = int("".join(condition))
-            while tape[tapeslct] != cond:
-                execute(loop)
-    elif cmd == "+":
+    if cmd == "+":
+        if debug == 1: print"inc"
         tape[tapeslct] += 1
-    elif cmd == "-":
+    if cmd == "-":
+        if debug == 1: print"dec"
         tape[tapeslct] -= 1
-    elif cmd == ">":
+    if cmd == ">":
+        if debug == 1: print"right"
         tapeslct += 1
         if tapeslct == len(tape):
             tape.append(0)
-    elif cmd == "<":
+    if cmd == "<":
+        if debug == 1: print"left"
         tapeslct -= 1
-    elif cmd == ".":
+    if cmd == ".":
+        if debug == 1: print"outASCII"
         sys.stdout.write(chr(tape[tapeslct]))
-    elif cmd == ":":
+    if cmd == ";":
+        if debug == 1: print"out"
         sys.stdout.write(tape[tapeslct])
-    elif cmd == ",":
-        tape[tapeslct] = ord(input())
-    elif cmd == ";":
-        tape[tapeslct] = int(input)
-    elif cmd == "[":
+    if cmd == ",":
+        if debug == 1: print"inASCII"
+        tape[tapeslct] = ord(stdin[0])
+        stdin.pop(0)
+    if cmd == "[":
+        if debug == 1: print"openloop"
         loops.append(cmdslct)
-    elif cmd == "]":
+    if cmd == "]":
+        if debug == 1: print"closeloop"
         latestloop = len(loops) - 1
         if tape[tapeslct] != 0:
             cmdslct = loops[latestloop]
         else:
             loops.pop(latestloop)
-    elif cmd == "|":
-        loop = program[cmdslct - 1]
-        condition = []
-        condparse = 1
-    if debug == 1:
-        print cmd
-        print tape
-
+    if cmd == "|":
+        if debug == 1: print"sieve"
+        sieve = []
+        cmdslct += 1
+        ctrl = 0
+        number = 0
+        while program[cmdslct] != "|":
+            sieve.append(program[cmdslct])
+            cmdslct += 1
+        for i in sieve:
+            if i == ">":
+                ctrl += 1
+            if i == "<":
+                ctrl -= 1
+            if i in "123456789":
+                number = 1
+                break
+        if number == 1:
+            until = int("".join(sieve))
+        else:
+            until = tape[tapeslct + ctrl]
+        if debug == 1: print until
+        cmdslct += 1
+        if program[cmdslct] == "{":
+            cmdslct += 1
+            func = []
+            while program[cmdslct] != "}":
+                func.append(program[cmdslct])
+                cmdslct += 1
+        else:
+            func = program[cmdslct]
+        while tape[tapeslct] != until:
+            if type(func) == type([]):
+                for x in func:
+                    execute(x)
+            else:
+                execute(func)
 while len(program) > cmdslct:
     execute(program[cmdslct])
     cmdslct += 1
-print "\n"
+print
 
